3fcaaf817f41f240ef1bb6ed243b6d7366510b97
sreeith <sreejith.js89@gmail.com>
2018-06-17 10:51:22 +0530

    my first commit

---
+++ src/test/java/samplejittest/samlejittest.java
@@ -0,0 +1,31 @@
+package samplejittest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import com.steadystate.css.util.Output;
+
+public class samlejittest {
+
+
+    @Test
+    public void logintest() throws IOException
+    {
+            WebDriver wd = new FirefoxDriver();
+            wd.manage().window().maximize();
+            wd.get("https://www.bing.com");
+            File src=((TakesScreenshot) wd).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(src, new
+File("C:\\Users\\sreejith\\workspace\\samplejittest\\screenshot\\test.jpg"));
+
+    }
+
+}
